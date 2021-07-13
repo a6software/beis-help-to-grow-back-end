143ee9978d1e1a7b649a9a6a7d4f1458d086a5d1
Christopher Moss <chris@a6software.co.uk>
2021-07-13 11:55:18 +0100

    Eligibility cyber sec validations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express';
 import initCreateAccountRouter from './create-account';
+import eligibilityRouter from './eligibility';
 import { UserService } from '../types';
 
 const initRootRouter = (userService: UserService): Router => {
   const router = express.Router();
 
   router.use('/create-account', initCreateAccountRouter(userService));
+  router.use('/eligibility', eligibilityRouter);
 
   return router;
 };
